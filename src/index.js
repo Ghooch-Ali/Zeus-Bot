11fd12fb4344d2f7843b8f967eb824b1d8fe70c6
Saeed-Paris <Saeed-Paris@noreply.com>
2023-05-20 01:16:44 +0330

    qwe

--- src/index.js
+++ src/index.js
@@ -8,10 +8,34 @@ const bot = new TelegramBot(token, { polling: true });
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
 
+  // Create the button
   const button = {
-    text: 'Click me!', 
+    text: 'Click me!',
     callback_data: 'button_click',
   };
 
-  bot.sendMessage(chatId, 'Hello! Welcome to your Telegram bot.');
+  // Create the inline keyboard markup
+  const keyboard = {
+    inline_keyboard: [[button]],
+  };
+
+  // Send the message with the button
+  bot.sendMessage(chatId, 'Press the button:', {
+    reply_markup: keyboard,
+  });
+});
+
+// Handle button clicks
+bot.on('callback_query', (query) => {
+  if (query.data === 'button_click') {
+    const chatId = query.message.chat.id;
+    const messageId = query.message.message_id;
+
+    // Respond to the button click
+    bot.sendMessage(chatId, 'Button clicked!');
+
+    // You can also edit or delete the original message if needed
+    // bot.editMessageText('New text', { chat_id: chatId, message_id: messageId });
+    // bot.deleteMessage(chatId, messageId);
+  }
 });
