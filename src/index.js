8e34775e0ac78d30be6c20d06b22915f8b97b786
Saeed-Paris <Saeed-Paris@noreply.com>
2023-05-20 01:12:43 +0330

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+console.log(`Hello Node.js v${process.versions.node}!`);
+import TelegramBot  from 'node-telegram-bot-api'
+
+const token = process.env.TELEGRAM_BOT_TOEKN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  const button = {
+    text: 'Click me!', 
+    callback_data: 'button_click',
+  };
+
+  bot.sendMessage(chatId, 'Hello! Welcome to your Telegram bot.');
+});
